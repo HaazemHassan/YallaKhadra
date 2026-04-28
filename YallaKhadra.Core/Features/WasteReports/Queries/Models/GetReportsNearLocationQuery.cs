043e29271f87fd57c545ba95852b19d46336e869
Ahmed Samy <ahmedsamylike@gmail.com>
2026-04-28 17:38:30 +0300

    add pagination in /WasteReport/near

--- YallaKhadra.Core/Features/WasteReports/Queries/Models/GetReportsNearLocationQuery.cs
+++ YallaKhadra.Core/Features/WasteReports/Queries/Models/GetReportsNearLocationQuery.cs
@@ -3,9 +3,11 @@ using YallaKhadra.Core.Bases.Responses;
 using YallaKhadra.Core.Features.WasteReports.Queries.Responses;
 
 namespace YallaKhadra.Core.Features.WasteReports.Queries.Models {
-    public class GetReportsNearLocationQuery : IRequest<Response<List<WasteReportResponse>>> {
+    public class GetReportsNearLocationQuery : IRequest<PaginatedResult<WasteReportResponse>> {
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
         public double RadiusInKm { get; set; } = 5.0; // Default 5 km
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
     }
 }
